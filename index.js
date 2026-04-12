de0d6ee09f33532be433c747265f0a1fa4138be1
luccasfes <lucs.silva11@gmail.com>
2026-04-11 23:13:25 -0300

    novo log

--- index.js
+++ index.js
@@ -133,6 +133,9 @@ client.on('messageCreate', async (message) => {
 });
 
 console.log("[DEBUG] TOKEN:", process.env.DISCORD_TOKEN ? "CARREGADO" : "UNDEFINED");
+process.on('unhandledRejection', (error) => {
+    console.error('[UNHANDLED]', error.message);
+});
 client.login(process.env.DISCORD_TOKEN)
   .then(() => console.log("[DEBUG] Login no Discord OK"))
   .catch(err => console.error("[DEBUG] Falha no login:", err.message));
